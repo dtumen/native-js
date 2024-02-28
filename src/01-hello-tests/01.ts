c378648febdecf257c0bbb7a2d2f6d1742abe669
Tumen Dambaev <dt1109@mail.ru>
2024-02-28 19:10:10 +0300

    smth done

---
+++ src/01-hello-tests/01.ts
@@ -0,0 +1,13 @@
+
+export function sum(a: number, b: number) {
+  return a + b;
+}
+
+export function mult(a: number, b: number) {
+  return a * b;
+}
+
+export function splitIntoWords(sentense: string ) {
+  return ['Hellow', 'my', 'friend!'];
+}
+
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { splitIntoWords } from './01-hello-tests/01';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
