f476fc79dfa57dc3293ff71b1155123d1277e59d
dtumen <dt1109@mail.ru>
2024-03-04 10:35:22 +0300

    lesson 1 done

--- src/01-hello-tests/01.test.ts
+++ src/01-hello-tests/01.test.ts
@@ -1,11 +1,17 @@
-import { sum } from './01';
+import { sum, mult, splitIntoWords } from './01';
+
+  //data:
+  let a: number;
+  let b: number;
+  let c: number;
+
+  beforeEach( () => {
+    a = 1;
+    b = 2;
+    c = 3;
+  })
 
 test('sum should be correct', () => {
-  //data:
-  const a = 1;
-  const b = 2;
-  const c = 3;
-
   //action:
   const result1 = sum(a, b);
   const result2 = sum(b, c);
@@ -14,5 +20,42 @@ test('sum should be correct', () => {
   //expect result:
   expect(result1).toBe(3);
   expect(result2).toBe(5);
-  expect(result2).not.toBe(4);
+  expect(result3).not.toBe(4);
+})
+
+test('multiply should be correct', () => {
+  //action:
+  const result1 = mult(a, b);
+  const result2 = mult(b, c);
+  const result3 = mult(b, c);
+
+  //expect result:
+  expect(result1).toBe(2);
+  expect(result2).toBe(6);
+  expect(result3).not.toBe(5);
+})
+
+test('splitting into words should be correct', () => {
+  // data:
+  const sentense1 = 'Hello my friend!';
+  const sentense2 = 'JS - the best  programming language.';
+
+  // action:
+  const result1 = splitIntoWords(sentense1);
+  const result2 = splitIntoWords(sentense2);
+
+  // expect:
+  expect(result1.length).toBe(3);
+  expect(result1[0]).toBe('hello');
+  expect(result1[1]).toBe('my');
+  expect(result1[2]).toBe('friend');
+
+  // 2:
+  expect(Array.isArray(result2)).toBeTruthy();
+  expect(result2.length).toBe(5);
+  expect(result2[0]).toBe('js');
+  expect(result2[1]).toBe('the');
+  expect(result2[2]).toBe('best');
+  expect(result2[3]).toBe('programming');
+  expect(result2[4]).toBe('language');
 })
--- src/01-hello-tests/01.ts
+++ src/01-hello-tests/01.ts
@@ -7,7 +7,9 @@ export function mult(a: number, b: number) {
   return a * b;
 }
 
-export function splitIntoWords(sentense: string ) {
-  return ['Hellow', 'my', 'friend!'];
+export function splitIntoWords(sentence: string) {
+  const words = sentence.match(/\b[\w']+\b/g);
+  return words ? words.map(word => word.toLowerCase()) : [];
 }
+
 
